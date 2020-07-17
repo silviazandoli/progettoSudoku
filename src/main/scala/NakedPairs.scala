925123020257d012b471f976d4b2ebc0142aeacb
albianto97 <albyantox@gmail.com>
2020-07-17 17:23:18 +0200

    NakedPairs

--- src/main/scala/NakedPairs.scala
+++ src/main/scala/NakedPairs.scala
@@ -1,31 +1,31 @@
-import scala.io.Source
-
-object NakedPairs  {
+object NakedPairs {
   val unit = 3 // larghezza blocco -> for che scorre di tre
-  import SudokuLoad.{puzzle,dimSudoku,display}
+
+  import SudokuLoad.{puzzle, dimSudoku, display}
   import SudokuMatrix.matList;
+  var e2lement = new Array[Int](2)
+
 
   def main(args: Array[String]): Unit = {
     println("start sudoku naked")
-    solve(0, 0)
+    solve()
     display()
   }
 
-  def solve(row: Int, col: Int): Boolean = {
-    if (puzzleSolved()) {
+  def solve(): Unit = {
+    /*if (puzzleSolved()) {
       return true
-    } else {
-      //for riga x colonna
-      for (i <- 0 until dimSudoku) {
-        for (j <- 0 until dimSudoku) {
-          if (matList(i)(j).size == 2) { // se non entra significa l'elenco ha un solo elemento all'interno
-                                          // deve essere assegnato al puzzle.
-            /*lista size*/
-            var number = new Array[Int](dimSudoku)
-
-
-
-          }
+    } else {*/
+    //for riga x colonna
+    for (i <- 0 until dimSudoku) {
+      for (j <- 0 until dimSudoku) {
+        if (matList(i)(j).size == 2) {
+          /*lista size*/
+          val number : Array[List[Int]]  = Array.ofDim[List[Int]](2)//array con i 2 numeri
+          var k: Int = 0 ;
+          number(k) = matList(i)(j)(k)
+          e2lement(k) = puzzle(i)(j)
+          k+=1;
         }
       }
       //for del blocco 3x3
@@ -34,19 +34,34 @@ object NakedPairs  {
           print("blocco")
         }
       }
-      false
-    }
-
-    def puzzleSolved(): Boolean = {
-      for (i <- 0 until dimSudoku) {
-        for (j <- 0 until dimSudoku) {
-          if (puzzle(i)(j) == 0) {
-            return false
-          }
-        }
-      }
-
-      true
     }
   }
+
+  def puzzleSolved(): Boolean = {
+    for (i <- 0 until dimSudoku) {
+      for (j <- 0 until dimSudoku) {
+        if (puzzle(i)(j) == 0) {
+          return false
+        }
+      }
+    }
+
+    true
+  }
 }
+  /*def solve(row: Int, col: Int): Boolean = {
+    var result: java.lang.Boolean = false
+    var k: Int = 0
+    while (k < 9 && !result) {
+      if (naked_pair_unit(puzzle(k) || naked_pair_unit(puzzle(k)) ||
+        naked_pair_unit(box(k))) {
+        result = true
+      }
+      { k += 1; k - 1 }
+    }
+    return result;
+  }
+  def naked_pair_unit(C: Int[9][2]): Unit ={
+
+  }*/
+
