7eb247f204b865b864bbbac9a101b9979ce6ad71
lorenz95 <lorenzMega95@outlook.it>
2020-07-19 11:26:58 +0200

    eliminato commento inutile

--- src/main/scala/FullExploration.scala
+++ src/main/scala/FullExploration.scala
@@ -1,27 +1,6 @@
-
-
 object FullExploration {
   import SudokuLoad.{puzzle,dimSudoku}
 
- /* def validate(row: Int, col: Int, num: Int): Boolean = {
-    for (i <- 0 until dimSudoku) {
-
-      if (puzzle(row)(i) == num || puzzle(i)(col) == num) {
-        return false
-      }
-    }
-
-    val r = (row / 3) * 3
-    val c = (col / 3) * 3
-    for (i <- r until r + 3) {
-      for (j <- c until c + 3) {
-        if (puzzle(i)(j) == num) {
-          return false
-        }
-      }
-    }
-    true
-  }*/
  def validate(position:(Int,Int),value:Int): Boolean = {
    //for each row, column and block 3*3
    val v=puzzle.transpose
@@ -30,7 +9,6 @@ object FullExploration {
    val squareCells = puzzle.grouped(3).toList(ci).flatMap { x => x.grouped(3).toList(cj) }
 
    !puzzle(position._1).contains(value) && !v(position._2).contains(value) && !squareCells.contains(value)
-
  }
 
   def puzzleSolved(): Boolean = puzzle.flatten.forall(_.!=(0))
