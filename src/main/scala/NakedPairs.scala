e11e7fdd357cea9cad2ebea3a9a3030eddf96e49
albianto97 <albyantox@gmail.com>
2020-07-20 15:36:58 +0200

    NakedPairs

--- src/main/scala/NakedPairs.scala
+++ src/main/scala/NakedPairs.scala
@@ -1,52 +1,28 @@
+import SudokuMatrix.matList
+import SudokuLoad.dimSudoku
+
+import scala.annotation.tailrec
 object NakedPairs {
-  val unit = 3 // larghezza blocco -> for che scorre di tre
 
-  import SudokuLoad.{puzzle, dimSudoku, display}
-  import SudokuMatrix.matList;
-  var e2lement = new Array[Int](2)
-
-
-  def main(args: Array[String]): Unit = {
-    println("start sudoku naked")
-    solve()
-    display()
-  }
-
-  def solve(): Unit = {
-    /*if (puzzleSolved()) {
-      return true
-    } else {*/
-    //for riga x colonna
-    for (i <- 0 until dimSudoku) {
-      for (j <- 0 until dimSudoku) {
-        if (matList(i)(j).size == 2) {
-          /*lista size*/
-          val number : Array[List[Int]]  = Array.ofDim[List[Int]](2)//array con i 2 numeri
-          var k: Int = 0 ;
-          number(k) = matList(i)(j)(k)
-          e2lement(k) = puzzle(i)(j)
-          k+=1;
+  def solve(row: Int, col: Int): Unit = {
+    for (i <- 0 until dimSudoku) { //per ogni riga
+      for (j <- 0 until dimSudoku) { // per ogni colonna
+        //controllo che sia di lunghezza
+        if (matList(i)(j).size == 2 && matList(i)(j) == matList(row)(col) && i != row && j != col) {
+          /*rimuovo da tutte le altre quei due elementi*/
+          for(k <- 0 until dimSudoku){
+            // TODO: Cancellare gli elementi che sono presenti ed uguali in matList di Row/Col e quelli
+            // di i/j da tutta la riga/colonna.
+          }
         }
       }
       //for del blocco 3x3
-      for (k <- 0 until unit) {
-        for (z <- 0 until unit) {
+      for (k <- 0 until dimSudoku) {
+        for (z <- 0 until dimSudoku) {
           print("blocco")
         }
       }
     }
-  }
-
-  def puzzleSolved(): Boolean = {
-    for (i <- 0 until dimSudoku) {
-      for (j <- 0 until dimSudoku) {
-        if (puzzle(i)(j) == 0) {
-          return false
-        }
-      }
-    }
-
-    true
   }
 }
   /*def solve(row: Int, col: Int): Boolean = {
