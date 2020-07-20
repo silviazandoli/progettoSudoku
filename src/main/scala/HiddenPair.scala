4d63460c42b815d4cdda60478a5a6f43d58dc235
Silvia <silvia.zandoli2@studio.unibo.it>
2020-07-20 13:58:24 +0200

    eliminato def inutile

--- src/main/scala/HiddenPair.scala
+++ src/main/scala/HiddenPair.scala
@@ -4,14 +4,9 @@ import SudokuLoad.dimSudoku
 import scala.annotation.tailrec
 
 object HiddenPair {
+  //A hidden pair occurs when a pair of numbers appears in exactly two squares in a row, column, or block,
+  // but those two numbers aren't the only ones in their squares.you can get rid of the other candidates in those squares
 
-  def ruleHiddenSet: Boolean = {
-    //A hidden pair occurs when a pair of numbers appears in exactly two squares in a row, column, or block,
-    // but those two numbers aren't the only ones in their squares.you can get rid of the other candidates in those squares
-
-
-    true
-  }
 
   /*
   implementazione provvisoria di trovare coppie nel puzzle
