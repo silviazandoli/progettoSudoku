23411d76fdc9aeb8833f70c92bd02ace83db6dce
lorenz95 <lorenzMega95@outlook.it>
2020-07-20 11:23:40 +0200

    implementazione provvisoria hiddenPair

--- src/main/scala/HiddenPair.scala
+++ src/main/scala/HiddenPair.scala
@@ -1,5 +1,8 @@
+import SudokuMatrix.matList
+import SudokuLoad.dimSudoku
 
-//file di prova, regola Hidden Pair/Triple
+import scala.annotation.tailrec
+
 object HiddenPair {
 
   def ruleHiddenSet: Boolean = {
@@ -8,6 +11,44 @@ object HiddenPair {
 
 
     true
+  }
+
+  /*
+  implementazione provvisoria di trovare coppie nel puzzle
+   */
+  @tailrec
+  def findCouplePuzzle(row: Int, col: Int): Unit = {
+    val listCouple = findCouple(row, col)
+
+    if (listCouple != Nil) {
+      listCouple(0)
+      listCouple(1)
+    }
+
+    if (row < dimSudoku) findCouplePuzzle(row + 3, col)
+  }
+
+  /*
+  data una cella cerca ogni possibile coppia nel suo sottoquadrato
+   */
+  def findCouple(row: Int, col: Int): List[(Int, Int)] = {
+    val r = (row / 3) * 3
+    val c = (col / 3) * 3
+
+    val coupleFirst = (row, col)
+    var coupleSecond = (row, col)
+
+    for {
+      i <- r until r + 3
+      j <- c until c + 3
+      if i != row && j != col
+    } yield {
+      if (matList(row)(col) == matList(i) (j)) {
+        coupleSecond = (i, j)
+        return List(coupleFirst, coupleSecond)
+      }
+    }
+    Nil
   }
 
 }
