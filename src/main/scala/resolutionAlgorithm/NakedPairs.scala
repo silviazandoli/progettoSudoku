fa98bda63a039b953488e349fca9443bb8893067
albianto97 <albyantox@gmail.com>
2020-07-21 15:29:09 +0200

    1) MatList deve arrivare già caricata delle possibili soluzoni; 2) Fare solve-> Naked che valga in ricorsione per tutte con aggiunta di un possibile array[List] che memorizzi gli indici i,j; 3) Funzione per eliminare elemento dalla lista con input il numero.

--- src/main/scala/resolutionAlgorithm/NakedPairs.scala
+++ src/main/scala/resolutionAlgorithm/NakedPairs.scala
@@ -5,16 +5,17 @@ import SudokuLoad.dimSudoku
 
 object NakedPairs {
 
+
   def solve(row: Int, col: Int): Unit = {
     for (i <- 0 until dimSudoku) { //per ogni riga
       for (j <- 0 until dimSudoku) { // per ogni colonna
-        //controllo che sia di lunghezza
+        //controllo che sia di lunghezza 2
         if (i != row && j != col && matList(i)(j).size == 2 && matList(i)(j) == matList(row)(col)) {
           /*rimuovo da tutte le altre quei due elementi*/
           for(k <- 0 until dimSudoku){
             for(number <- matList(i)(k)){
-              if (number == matList(i)(j)(0) || number == matList(i)(j)(1)){
-                /* rimuovere elemento dalla lista*/
+              if (number == matList(i)(j).head || number == matList(i)(j)(1)){
+                /* rimuovere elemento dalla lista*/ /* TODO: FARE UNA FUNZIONE APPOSITA*/
                 matList(i)(k) = Nil
               }
             }
@@ -29,4 +30,13 @@ object NakedPairs {
       }*/
     }
   }
+  /*def createMatlist(puzzle: Array[Array[Int]]) = {
+    for(i<-0 until dimSudoku) {
+      for(j<-0 until dimSudoku) {
+        matList(i)(j)=solve(i,j,puzzle)
+        println(" matList nella posizione " + i + "," + j+ " è " + matList(i)(j))
+      }
+    }
+
+  }*/
 }
