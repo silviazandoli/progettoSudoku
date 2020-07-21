bc3655203cdf988dc32592de824df64c4675010c
albianto97 <albyantox@gmail.com>
2020-07-21 17:49:04 +0200

    Naked Ricorsiva, da finire

--- src/main/scala/resolutionAlgorithm/NakedPairs.scala
+++ src/main/scala/resolutionAlgorithm/NakedPairs.scala
@@ -5,8 +5,11 @@ import SudokuLoad.dimSudoku
 
 object NakedPairs {
 
+  val matList: Array[Array[List[Int]]] = Array.ofDim[List[Int]](dimSudoku, dimSudoku)
+  val list: Array[List[Int]] = Array.ofDim[List[Int]](2)
 
-  def solve(row: Int, col: Int): Unit = {
+  // TODO: SOLUZIONE 1 QUELLA INIZIALE
+  /*def solve(row: Int, col: Int): Unit = {
     for (i <- 0 until dimSudoku) { //per ogni riga
       for (j <- 0 until dimSudoku) { // per ogni colonna
         //controllo che sia di lunghezza 2
@@ -29,14 +32,40 @@ object NakedPairs {
         }
       }*/
     }
+  }*/
+
+  // TODO : VARIANTE SUCCESSIVA
+  def solve(row: Int, col: Int): Unit = {
+    cycle()
+    //controllo che sia di lunghezza 2
+    if (matList(row)(col).size == 2) {
+      val i:Int =0
+      list(i).toSet(row) //-> aggiunta di riga e colonna nella lista
+      list(i).toSet(col)
+      /*rimuovo da tutte le altre quei due elementi*/
+      if(list.size == 2) {
+        for (k <- 0 until dimSudoku) {
+          for (number <- matList(row)(k)) {
+            if (number == matList(row)(col)(0) || number == matList(row)(col)(1)) {
+              /* rimuovere elemento dalla lista*/
+              /* TODO: FARE UNA FUNZIONE APPOSITA*/
+              matList(row)(k) = Nil
+            }
+          }
+        }
+        list = list.filter(_ > 0)
+        list = list.
+      }
+    }
   }
-  /*def createMatlist(puzzle: Array[Array[Int]]) = {
+
+  def cycle() = {
     for(i<-0 until dimSudoku) {
       for(j<-0 until dimSudoku) {
-        matList(i)(j)=solve(i,j,puzzle)
+        solve(i,j)
         println(" matList nella posizione " + i + "," + j+ " è " + matList(i)(j))
       }
     }
 
-  }*/
+  }
 }
