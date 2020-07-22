8d2661cc07e03035d0acc95a2202b8610eed4d63
lorenz95 <lorenzMega95@outlook.it>
2020-07-22 12:59:44 +0200

    passato testHiddenSingle

--- src/test/scala/testHiddenSingles/testHiddenSingles.scala
+++ src/test/scala/testHiddenSingles/testHiddenSingles.scala
@@ -5,13 +5,15 @@ import sudoku.SudokuLoad.loadPuzzle
 import sudoku.MatListOperation.initList
 import utility.getPuzzle
 
+import resolutionAlgorithm.FullExploration.solve
 import resolutionAlgorithm.HiddenSingles.hiddenSingles
 
 class testHiddenSingles extends FunSuite {
-  val nameFile = "input/sudoku01.txt"
-  val nameSolved = "outputSolved/sudoku01.txt"
 
-  test("") {
+  test("Sudoku01") {
+    val nameFile = "input/sudoku01.txt"
+    val nameSolved = "outputSolved/sudoku01.txt"
+
     loadPuzzle(nameFile)
     initList()
     hiddenSingles(1, 1)
@@ -21,5 +23,19 @@ class testHiddenSingles extends FunSuite {
     val sudokuSolved = getPuzzle
 
     assert(sudokuInput(1)(1) == sudokuSolved(1)(1))
+  }
+
+  test("Sudoku06") {
+    val nameFile = "input/sudoku06.txt"
+
+    loadPuzzle(nameFile)
+    initList()
+    hiddenSingles(7, 0)
+    val sudokuInput = getPuzzle
+
+    solve(0,0)
+    val sudokuSolved = getPuzzle
+
+    assert(sudokuInput(7)(0) == sudokuSolved(7)(0))
   }
 }
