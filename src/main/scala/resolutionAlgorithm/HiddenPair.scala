ea41aa2bc212ecbf842485b98c27ec98dc29396d
Silvia <silvia.zandoli2@studio.unibo.it>
2020-07-24 20:14:45 +0200

    hidden pair continuo

--- src/main/scala/resolutionAlgorithm/HiddenPair.scala
+++ src/main/scala/resolutionAlgorithm/HiddenPair.scala
@@ -1,5 +1,7 @@
 package resolutionAlgorithm
+
 import utility.{dimSudoku, matList}
+
 /*A hidden pair occurs when a pair of numbers appears in exactly
  two squares in a row, column, or block, but those two numbers aren't the only ones in their squares.
  */
@@ -7,20 +9,84 @@ import utility.{dimSudoku, matList}
 //manca il blocco
 //fare la cosa dell'eliminazione (con l'updateList che c'è nel  progetto)
 object HiddenPair {
-
   def solveHiddenPair(): Unit = {
     val rows = (0 until dimSudoku).toList.map(checkRow)
-    println(rows)
+    rows.zipWithIndex.foreach(t => {
+      val row = t._2
+      t._1.foreach(p => {
+        val newValues = p.intersection.head.toList
+        matList(row)(p.cell1) = newValues
+        matList(row)(p.cell2) = newValues
+      })
+    })
+    // println(rows)
 
     val cols = (0 until dimSudoku).toList.map(checkColumn)
-    println(cols)
+    cols.zipWithIndex.foreach(t => {
+      val col = t._2
+      t._1.foreach(p => {
+        val newValues = p.intersection.head.toList
+        matList(p.cell1)(col) = newValues
+
+        matList(p.cell2)(col) = newValues
+      })
+    })
+    //  println(cols)
+    //  println(matList.toList)
+
+
+    val blocks = (0 until dimSudoku).toList.map(checkBlock)
+    blocks.zipWithIndex.foreach(t => {
+      val block = t._2
+      var shiftRow = 0
+      var shiftCol = 0
+      block match {
+        case 0 | 1 | 2 => {
+          shiftRow = 0
+          shiftCol = block * 3
+        }
+        case 3 | 4 | 5 => {
+          shiftRow = 3
+          shiftCol = (block - 3) * 3
+        }
+        case 6 | 7 | 8 => {
+          shiftRow = 6
+          shiftCol = (block - 6) * 3
+        }
+      }
+      t._1.foreach(p => {
+        List(p.cell1, p.cell2).foreach(c => {
+          val converted = base3(c)
+          val ct = (converted._1 + shiftRow, converted._2 + shiftCol)
+          matList(ct._1)(ct._2) = p.intersection.head.toList
+        })
+
+
+      })
+    })
+    //println(blocks)
+
+  }
+
+  def base3(num: Int): (Int, Int) = {
+    val one = num / 3
+    val first = num % 3
+    val second = one % 3
+    (second, first)
   }
 
   def checkRow(row: Int) = {
 
     //it gets the squares of the matList which have more than one element
     val ml = matList(row).toList.zipWithIndex.filter(_._1.size > 1)
+    // val pippo:Nothing=ml
+    check(ml)
 
+  }
+
+  def check(ml: List[(List[Int], Int)]): List[PossiblePair] = {
+
+    // val pippo:Nothing=ml
     //possiblePairs contains all the possibles pairs with their coordinates
     val possiblePairs = ml.map(couples).toSet.subsets(2).toList.map(e => {
       val val1 = e.head
@@ -54,46 +120,14 @@ object HiddenPair {
 
     })
     hiddenPairs
-
   }
 
   def checkColumn(col: Int) = {
 
     //it gets the squares of the matList which have more than one element
-    val ml = matList.map(_ (col)).zipWithIndex.filter(_._1.size > 1)
-    println(ml.toList)
-    //possiblePairs contains all the possibles pairs with their coordinates
-    val possiblePairs = ml.map(couples).toSet.subsets(2).toList.map(e => {
-      val val1 = e.head
-      val val2 = e.tail.head
-      val intersection = val1._1.toSet.intersect(val2._1.toSet)
-      PossiblePair(val1._2, val2._2, intersection)
-
-    }).filter(_.intersection.nonEmpty)
-
-    //there are some cases as List(1,7,Set(Set(4,3),Set(5,6)). From row 34 to 38
-    //I make sure to separate the different pairs in order to get
-    // List(1,7,Set(Set(4,3))) e List(1,7,Set(Set(5,6)))
-    val flattenPairs = possiblePairs.map(p => {
-      p.intersection.map(pair => {
-        PossiblePair(p.cell1, p.cell2, Set(pair))
-      })
-    }).flatten
-    //it finds the hidden pair
-    val hiddenPairs = flattenPairs.filter(p => {
-      val cell1 = p.cell1
-      val cell2 = p.cell2
-      val exclusion = ml.filter(_._2 != cell1).filter(_._2 != cell2).map(e => e._1.toSet).flatten.toSet
-      //we have to compare inside intersection elem per elem. We get the first
-      val c1 = exclusion.contains(p.intersection.head.head)
-      //we have to compare inside intersection elem per elem. We get the second. tail contains more element so we get the head
-      val c2 = exclusion.contains(p.intersection.head.tail.head)
-
-      !c1 && !c2
-
-
-    })
-    hiddenPairs
+    val ml = matList.map(_ (col)).zipWithIndex.filter(_._1.size > 1).toList
+    //  println(ml.toList)
+    check(ml)
 
   }
 
@@ -127,14 +161,25 @@ object HiddenPair {
 
 
     } yield (i, j)
-    val ml = square.map(p => (matList(p._1)(p._2), (p._1 + p._2) % 9))
+    // println(square)
+    val h = square.head
+
+    def calcindex(p1: Int, p2: Int): Int = {
+      val pp1 = p1 - h._1
+      val pp2 = p2 - h._2
+      //pp1*3^1+ pp2*3^0
+      pp1 * 3 + pp2
+    }
+
+    val ml = square.map(p => (matList(p._1)(p._2), calcindex(p._1, p._2))).toList
+    // println(ml)
+    check(ml)
 
 
   }
 
 
   // updateList(rowCol: (Int, Int), elem: Int): aggiorna giÃ  la matList e elimina gli elementi Ã¨ in sudoku matrix
-
 
 
   // updateList(rowCol: (Int, Int), elem: Int): aggiorna già  la matList e elimina gli elementi ¨ in sudoku matrix
--- src/test/scala/testHiddenPairs/TestHiddenPair.scala
+++ src/test/scala/testHiddenPairs/TestHiddenPair.scala
@@ -4,7 +4,7 @@ import org.scalatest.FunSuite
 import resolutionAlgorithm.HiddenPair
 import sudoku.MatListOperation
 import sudoku.SudokuLoad.loadPuzzle
-import utility.{display, puzzle}
+import utility.{display, puzzle,matList}
 
 class TestHiddenPair extends FunSuite {
 
@@ -17,36 +17,41 @@ class TestHiddenPair extends FunSuite {
     display(puzzle)
     MatListOperation.initList()
     HiddenPair.solveHiddenPair()
-    HiddenPair.checkBlock(8)
+    HiddenPair.checkBlock(3)
+    println(HiddenPair.base3(8))
     /*Risolvere in maniera del genere*/
-   /* val sudokuInput = getPuzzle
+   /* initList()
+    hiddenSingles(1, 1)
+    val sudokuInput = getPuzzle
+
+    loadPuzzle(nameSolved)
+    val sudokuSolved = getPuzzle
+
+    assert(sudokuInput(1)(1) == sudokuSolved(1)(1))*/
+
+  }
+   //todo TEST SIMILE DA FARE
+  /*
+    test("Sudoku05") {
+    val nameFile = "input/sudoku05.txt"
+
+    loadPuzzle(nameFile)
+    initList()
+    totalHiddenSingles()
+    val sudokuInput = getPuzzle
 
     solve(0,0)
     val sudokuSolved = getPuzzle
 
-    assert(sudokuInput(7)(0) == sudokuSolved(7)(0))*/
-
+    assert(sudokuInput(2)(2) == sudokuSolved(2)(2))
+    assert(sudokuInput(5)(2) == sudokuSolved(5)(2))
   }
-    //verify bisogna vedere che funzioni e che si riesca a risolvere l'algoritmo
-    //test da fare
-  //todo il test è da fare così. come nel caso hidden single
-   /* test("Sudoku01") {
-      val nameFile = "input/sudoku01.txt"
-      val nameSolved = "outputSolved/sudoku01.txt"
+   */
+test("test block"){
+  println(matList.toList)
 
-      loadPuzzle(nameFile)
-      initList()
-      hiddenSingles(1, 1)
-      val sudokuInput = getPuzzle
+}
 
-      loadPuzzle(nameSolved)
-      val sudokuSolved = getPuzzle
-
-      assert(sudokuInput(1)(1) == sudokuSolved(1)(1))
-
-    //verify
-
-  }*/
 
 
 }
