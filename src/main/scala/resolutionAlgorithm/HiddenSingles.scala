443407fd43801092dbd8199052d27b33b8aec9d1
lorenz95 <lorenzMega95@outlook.it>
2020-07-25 12:54:10 +0200

    modificato HiddenSingle

--- src/main/scala/resolutionAlgorithm/HiddenSingles.scala
+++ src/main/scala/resolutionAlgorithm/HiddenSingles.scala
@@ -5,15 +5,18 @@ import sudoku.MatListOperation.updateList
 
 object HiddenSingles {
 
-  def totalHiddenSingles(): Unit =
-    for (i <- 0 until dimSudoku; j <- 0 until dimSudoku; if matList(i)(j) != Nil) hiddenSingles(i, j)
+  def totalHiddenSingles(): Unit = {
+    (0 until dimSudoku).foreach(i => (0 until dimSudoku).foreach(j => {
+      if (matList(i)(j) != Nil) hiddenSingles(i, j)
+    }))
+  }
 
   def hiddenSingles(row: Int, col: Int): Unit = {
-    for (elem <- matList(row)(col)
-         if searchInRow(row, elem) || searchInColumn(col, elem) || searchInSquare(row, col, elem)) {
-      updateList((row, col), elem)
-      puzzle(row)(col) = elem
-    }
+    matList(row)(col).filter(elem => searchInRow(row, elem) || searchInColumn(col, elem) || searchInSquare(row, col, elem))
+      .foreach(elem => {
+        updateList((row, col), elem)
+        puzzle(row)(col) = elem
+      })
   }
 
   def caseFound(numFound : Int): Boolean = {
@@ -27,7 +30,7 @@ object HiddenSingles {
     var valFound = 0
 
     def closureFound(index: Int): Unit = if (flag) {
-      if (matList(rowCol)(index) != Nil && matList(rowCol)(rowCol).contains(num)) {
+      if (matList(rowCol)(index) != Nil && matList(rowCol)(index).contains(num)) {
         valFound = valFound + 1
       }
     } else {
@@ -54,16 +57,10 @@ object HiddenSingles {
   }
 
   def searchInSquare(row: Int, col: Int, num: Int): Boolean = {
-      val r = (row / 3) * 3
-      val c = (col / 3) * 3
+    val ci = row / 3
+    val cj = col / 3
+    val squareCells = matList.grouped(3).toList(ci).flatMap { x => x.grouped(3).toList(cj) }
 
-      var numFound = 0
-
-      /*
-      aggiornamento sotto - quadrato
-       */
-    for (i <- r until r + 3; j <- c until c + 3; if matList(i)(j) != Nil && matList(i)(j).contains(num)) numFound = numFound + 1
-
-    caseFound(numFound)
+    caseFound(squareCells.filterNot(list => list == Nil).count(list => list.contains(num)))
   }
 }
