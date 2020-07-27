6f45de5faba53e6a7e506737396a19f0f153fecf
albianto97 <albyantox@gmail.com>
2020-07-27 15:47:03 +0200

    Naked Pairs Variant

--- src/main/scala/resolutionAlgorithm/NakedPairsVariant.scala
+++ src/main/scala/resolutionAlgorithm/NakedPairsVariant.scala
@@ -1,24 +1,26 @@
 package resolutionAlgorithm
 
 import utility.{dimSudoku, matList}
+import sudoku.MatListOperation.updateList
+
 
 object NakedPairsVariant {
 
   var coupleFound = (-1, -1)
 
   def findCouple(rowCol: Int, flag: Boolean): Unit = if (flag) {
-    val posRows = findCoupleInRow(rowCol)
-    val first = posRows._1
-    val second = posRows._2
+    val posRows = findCoupleInRow(rowCol) //ha una coppia di posizioni
+    val first = posRows._1 //primo elemento di posRows
+    val second = posRows._2 //secondo elemento di posRows
 
-    if (first != -1) {updateRowList(first, second, coupleFound)}
+    if (first != -1) {updateRowList(rowCol, first, second, coupleFound)}
 
   } else {
     val posCol = findCoupleInColum(rowCol)
     val first = posCol._1
     val second = posCol._2
 
-    if (first != -1) {updateColList(first, second, coupleFound)}
+    if (first != -1) {updateColList(rowCol,first, second, coupleFound)}
   }
 
   def findCoupleSubSquare(row1: Int, row2: Int) = {
@@ -29,23 +31,26 @@ object NakedPairsVariant {
     var found = 0
     var fist = -1
     var second = -1
-
     (1 to 8).foreach(i => {
       (2 to 9).foreach(j => {
         for {
           k <- 0 until dimSudoku
-          if matList(row)(k) != Nil && matList(row)(k).contains(i) && matList(row)(k).contains(j)
+          if matList(row)(k) != Nil && i!=j && matList(row)(k).contains(i) && matList(row)(k).contains(j)
         } {
           found = found + 1
 
-          if (found == 1) {fist = k
-            coupleFound = (i, j)}
-          if (found == 2) second = k
+          if (found == 1) {
+            fist = k             // prima volta entra se trova una coppia e assegna quella k a first
+            coupleFound = (i, j) //contiene i due numeri che sono uguali
+          }
+          if (found == 2) second = k // seconda coppia, assegna second
         }
       })
     })
+    println("first: "+ fist + " second: "+second)
+    println("coupleFound" + coupleFound)
     found match {
-      case 2 => (fist, second)
+      case 2 => (fist, second) //restituisce le posizioni
       case _ => (-1, -1)
     }
   }
@@ -63,8 +68,10 @@ object NakedPairsVariant {
         } {
           found = found + 1
 
-          if (found == 1) {fist = k
-            coupleFound = (i, j)}
+          if (found == 1) {
+            fist = k
+            coupleFound = (i, j)
+          }
           if (found == 2) second = k
         }
       })
@@ -75,12 +82,34 @@ object NakedPairsVariant {
     }
   }
 
-  def updateRowList(first: Int, second: Int, coupleFound: (Int, Int)) = {
-
+  def updateRowList(row: Int ,first: Int, second: Int, coupleFound: (Int, Int)) = {
+    //first è la prima posizione di quel numero
+    //second è la seconda k
+    //coupleFound ??
+    val n1 = coupleFound._1
+    println("elemento 1: "+n1)
+    val n2 = coupleFound._2
+    println("elemento 2: "+n2)
+    for {k <- 0 until (dimSudoku)} {
+      if (k != first && k != second) {
+        println("elimino elemento 1: "+n1 + "da " +row + " - "+ k)
+        updateList((row, k), n1)
+        updateList((row, k), n2)
+      }
+    }
   }
 
-  def updateColList(first: Int, second: Int, coupleFound: (Int, Int)) = {
-
+  def updateColList(col: Int, first: Int, second: Int, coupleFound: (Int, Int)) = {
+    val n1 = coupleFound._1
+    println("elemento 1: "+n1)
+    val n2 = coupleFound._2
+    println("elemento 2: "+n2)
+    for {k <- 0 until (dimSudoku)} {
+      if (k != first && k != second) {
+        println("elimino elemento 1: "+n1 + "da " +k + " - "+ col)
+        updateList((k, col), n1)
+        updateList((k, col), n2)
+      }
+    }
   }
-
 }
--- src/test/scala/testFull/TestNakedPair.scala
+++ src/test/scala/testFull/TestNakedPair.scala
@@ -1,34 +1,212 @@
 package testFull
 
 import org.scalatest.FunSuite
-import sudoku.MatListOperation.initList
+import resolutionAlgorithm.FullExploration.solve
 import sudoku.SudokuLoad.loadPuzzle
-import utility.{display, getPuzzle, puzzle}
+import sudoku.MatListOperation.initList
+import utility.getPuzzle
+import resolutionAlgorithm.NakedPairsVariant._
+import utility._
 
 
 
 class TestNakedPair extends FunSuite {
+  test("Sudoku01") {
 
-  /*test("Sudoku01") {
-    val nameFile = "input/sudoku01.txt"
+    val nameFile = "input/sudokuText.txt"
     //val nameSolved = "outputSolved/sudoku01.txt"
+    loadPuzzle(nameFile)
+
+    display(puzzle)
+    initList()
+    //for (i <- 0 to 8) {
+      for (j <- 0 to 8) {
+        println("MatList riga " + 1 + " colonna" + j + " è " + matList(1)(j).toString)
+      }
+    //}
+
+    val initMat = utility.matList(1)(8)
+    //printMatrix()
+    findCouple(1,true)
+    //findCouple(1,false)
+    //HiddenPair.checkBlock(3)
+    // println(HiddenPair.base3(8))
+    //for (i <- 0 to 8) {
+      for (j <- 0 to 8){
+        println("MatList DOPO riga " + 1 + " colonna" + j + " è " + matList(1)(j).toString)
+     }
+    //}
+
+    val finishMat = utility.matList(1)(8)
+
+    //assert(sudokuInput(1)(1) != sudokuSolved(1)(1))
+    println("matList iniziale " + initMat)
+    println("matList finale "+ finishMat)
+    assert(initMat != finishMat)
+  }
+
+  /*test("Naked Pair Sudoku02") {
+    //init
+    val nameFile = "input/sudoku02.txt"
+    val nameSolved = "outputSolved/sudoku02.txt"
+    loadPuzzle(nameFile)
+
+    display(puzzle)
+    initList()
+    for (i <- 0 to 8) {
+      for (j <- 0 to 8)
+        println("MatList riga " + i + " colonna" + j + " è " + matList(i)(j).toString)
+
+    }
+    val initMat = utility.matList(0)(8)
+    //printMatrix()
+    solveNakedPair()
+
+    //HiddenPair.checkBlock(3)
+    // println(HiddenPair.base3(8))
+    for (i <- 0 to 8) {
+      for (j <- 0 to 8)
+        println("MatList DOPO riga " + i + " colonna" + j + " è " + matList(i)(j).toString)
+    }
+    val finishMat = utility.matList(0)(8)
+
+    println("matList iniziale " + initMat)
+    println("matList finale "+ finishMat)
+    assert(initMat != finishMat)
+  }
+  test("Sudoku01") {
+    val nameFile = "input/sudoku01.txt"
 
     loadPuzzle(nameFile)
     initList()
-    val initMat = utility.matList(1)(7)
-    cycle()
+    solveNakedPair()
     val sudokuInput = getPuzzle
 
-    //loadPuzzle(nameSolved)
-    print(sudokuInput)
-    //val sudokuSolved = getPuzzle
-    val finishMat = utility.matList(1)(7)
+    solve(0, 0)
+    val sudokuSolved = getPuzzle
 
-    //assert(sudokuInput(1)(1) != sudokuSolved(1)(1))
-    println("matList iniziale" + initMat)
-    println("matList finale "+ finishMat)
-    assert(initMat == finishMat)
-  }*/
+    assert(sudokuInput(2)(2) == sudokuSolved(2)(2))
+    assert(sudokuInput(5)(2) == sudokuSolved(5)(2))
+
+  }
+  test("Sudoku05") {
+    val nameFile = "input/sudoku05.txt"
+
+    loadPuzzle(nameFile)
+    initList()
+    solve(0, 0)
+    val sudokuSolvedwithout = getPuzzle
+
+    loadPuzzle(nameFile)
+    initList()
+    solveNakedPair()
+    solve(0, 0)
+    val sudokuSolvedWith = getPuzzle
+    assert(sudokuSolvedwithout(2)(2) == sudokuSolvedWith(2)(2))
+    assert(sudokuSolvedWith(5)(2) == sudokuSolvedWith(5)(2))
+  }
+
+  test("Sudoku06") {
+    val nameFile = "input/sudoku06.txt"
+
+    loadPuzzle(nameFile)
+    initList()
+    solve(0, 0)
+    val sudokuSolvedwithout = getPuzzle
+
+    loadPuzzle(nameFile)
+    initList()
+    solveNakedPair()
+    solve(0, 0)
+    val sudokuSolvedWith = getPuzzle
+    assert(sudokuSolvedwithout(2)(2) == sudokuSolvedWith(2)(2))
+    assert(sudokuSolvedWith(5)(2) == sudokuSolvedWith(5)(2))
+  }
+
+  test("Sudoku11") {
+    val nameFile = "input/sudoku11.txt"
+
+    loadPuzzle(nameFile)
+    initList()
+    solve(0, 0)
+    val sudokuSolvedwithout = getPuzzle
+
+    loadPuzzle(nameFile)
+    initList()
+    solveNakedPair()
+    solve(0, 0)
+    val sudokuSolvedWith = getPuzzle
+    assert(sudokuSolvedwithout(2)(2) == sudokuSolvedWith(2)(2))
+    assert(sudokuSolvedWith(5)(2) == sudokuSolvedWith(5)(2))
+  }
+
+  test("Sudoku22") {
+    val nameFile = "input/sudoku22.txt"
+
+    loadPuzzle(nameFile)
+    initList()
+    solve(0, 0)
+    val sudokuSolvedwithout = getPuzzle
+
+    loadPuzzle(nameFile)
+    initList()
+    solveNakedPair()
+    solve(0, 0)
+    val sudokuSolvedWith = getPuzzle
+    assert(sudokuSolvedwithout(2)(2) == sudokuSolvedWith(2)(2))
+    assert(sudokuSolvedWith(5)(2) == sudokuSolvedWith(5)(2))
+  }
+
+  test("Sudoku04") {
+    val nameFile = "input/sudoku04.txt"
+
+    loadPuzzle(nameFile)
+    initList()
+    solve(0, 0)
+    val sudokuSolvedwithout = getPuzzle
+
+    loadPuzzle(nameFile)
+    initList()
+    solveNakedPair()
+    solve(0, 0)
+    val sudokuSolvedWith = getPuzzle
+    assert(sudokuSolvedwithout(1)(2) == sudokuSolvedWith(1)(2))
+    assert(sudokuSolvedWith(5)(3) == sudokuSolvedWith(5)(3))
+  }
+
+  test("Sudoku03") {
+    val nameFile = "input/sudoku03.txt"
+
+    loadPuzzle(nameFile)
+    initList()
+    solve(0, 0)
+    val sudokuSolvedwithout = getPuzzle
+
+    loadPuzzle(nameFile)
+    initList()
+    solveNakedPair()
+    solve(0, 0)
+    val sudokuSolvedWith = getPuzzle
+    assert(sudokuSolvedwithout(1)(2) == sudokuSolvedWith(1)(2))
+    assert(sudokuSolvedWith(5)(3) == sudokuSolvedWith(5)(3))
+  }
+
+  test("Sudoku23") {
+    val nameFile = "input/sudoku23.txt"
+
+    loadPuzzle(nameFile)
+    initList()
+    solve(0, 0)
+    val sudokuSolvedwithout = getPuzzle
+
+    loadPuzzle(nameFile)
+    initList()
+    solveNakedPair()
+    solve(0, 0)
+    val sudokuSolvedWith = getPuzzle
+    assert(sudokuSolvedwithout(1)(2) == sudokuSolvedWith(1)(2))
+    assert(sudokuSolvedWith(5)(3) == sudokuSolvedWith(5)(3))
+  }
 
   test("Sudoku01") {
     val nameFile = "input/sudoku01.txt"
@@ -38,6 +216,6 @@ class TestNakedPair extends FunSuite {
     val sudokuInput = getPuzzle
     print(sudokuInput)
     //NakedPairs.nakedPair(0,0)
-  }
+  }*/
 
 }
