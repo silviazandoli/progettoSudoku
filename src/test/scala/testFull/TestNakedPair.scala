2398e0f7eea15c011cadf498eed42eb1aea1e60c
albianto97 <albyantox@gmail.com>
2020-07-27 16:36:18 +0200

    Naked Pairs Variant Righe e Colonne, piccolo bug elimina gli elementi di quella riga/colonna

--- src/test/scala/testFull/TestNakedPair.scala
+++ src/test/scala/testFull/TestNakedPair.scala
@@ -14,59 +14,44 @@ class TestNakedPair extends FunSuite {
   test("Sudoku01") {
 
     val nameFile = "input/sudokuText.txt"
-    //val nameSolved = "outputSolved/sudoku01.txt"
     loadPuzzle(nameFile)
 
     display(puzzle)
     initList()
-    //for (i <- 0 to 8) {
-      for (j <- 0 to 8) {
-        println("MatList riga " + 0 + " colonna" + j + " è " + matList(0)(j).toString)
-      }
-    //}
+    for (i <- 0 to 8) {
+      println("MatList riga " + 0 + " colonna" + i + " è " + matList(0)(i).toString)
+    }
 
     val initMat = utility.matList(0)(4)
-    //printMatrix()
     findCouple(0,true)
-    //findCouple(1,false)
-    //HiddenPair.checkBlock(3)
-    // println(HiddenPair.base3(8))
-    //for (i <- 0 to 8) {
-      for (j <- 0 to 8){
-        println("MatList DOPO riga " + 0 + " colonna" + j + " è " + matList(0)(j).toString)
-     }
-    //}
+    for (i <- 0 to 8) {
+      println("MatList DOPO riga " + 0 + " colonna" + i + " è " + matList(0)(i).toString)
+    }
 
     val finishMat = utility.matList(0)(4)
 
-    //assert(sudokuInput(1)(1) != sudokuSolved(1)(1))
     println("matList iniziale " + initMat)
     println("matList finale "+ finishMat)
     assert(initMat != finishMat)
   }
 
-  /*test("Naked Pair Sudoku02") {
+  test("Naked Pair Sudoku02") {
     //init
-    val nameFile = "input/sudoku02.txt"
+    val nameFile = "input/sudoku04.txt"
     val nameSolved = "outputSolved/sudoku02.txt"
     loadPuzzle(nameFile)
 
     display(puzzle)
     initList()
     for (i <- 0 to 8) {
-      for (j <- 0 to 8)
-        println("MatList riga " + i + " colonna" + j + " è " + matList(i)(j).toString)
-
+      println("MatList riga " + i + " colonna" + 0 + " è " + matList(i)(0).toString)
     }
     val initMat = utility.matList(0)(8)
     //printMatrix()
-    solveNakedPair()
+    findCouple(1,false)
 
-    //HiddenPair.checkBlock(3)
-    // println(HiddenPair.base3(8))
     for (i <- 0 to 8) {
-      for (j <- 0 to 8)
-        println("MatList DOPO riga " + i + " colonna" + j + " è " + matList(i)(j).toString)
+      println("MatList DOPO riga " + i + " colonna" + 0 + " è " + matList(i)(0).toString)
     }
     val finishMat = utility.matList(0)(8)
 
@@ -74,21 +59,22 @@ class TestNakedPair extends FunSuite {
     println("matList finale "+ finishMat)
     assert(initMat != finishMat)
   }
-  test("Sudoku01") {
+  test("Sudoku") {
     val nameFile = "input/sudoku01.txt"
 
     loadPuzzle(nameFile)
     initList()
-    solveNakedPair()
+    findCouple(0,true)
     val sudokuInput = getPuzzle
 
     solve(0, 0)
     val sudokuSolved = getPuzzle
 
-    assert(sudokuInput(2)(2) == sudokuSolved(2)(2))
+    assert(sudokuInput(0)(4) == sudokuSolved(0)(4))
     assert(sudokuInput(5)(2) == sudokuSolved(5)(2))
 
   }
+  /*
   test("Sudoku05") {
     val nameFile = "input/sudoku05.txt"
 
