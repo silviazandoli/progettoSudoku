485a97eb8838595407a2fa223d4a91439d8e315d
Silvia <silvia.zandoli2@studio.unibo.it>
2020-07-27 18:47:31 +0200

    cancellata parte inutile

--- src/test/scala/testFull/TestNakedPair.scala
+++ src/test/scala/testFull/TestNakedPair.scala
@@ -13,7 +13,7 @@ import utility._
 class TestNakedPair extends FunSuite {
   test("Sudoku01") {
 
-    val nameFile = "input/sudokuText.txt"
+    val nameFile = "input/sudoku01.txt"
     loadPuzzle(nameFile)
 
     display(puzzle)
