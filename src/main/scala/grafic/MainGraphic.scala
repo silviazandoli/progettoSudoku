60bc9ccca504dc856f02bee7b5e1bf185260dc25
lorenz95 <lorenzMega95@outlook.it>
2020-07-29 16:08:35 +0200

    eliminati commenti inutili

--- src/main/scala/grafic/MainGraphic.scala
+++ src/main/scala/grafic/MainGraphic.scala
@@ -24,17 +24,5 @@ object MainGraphic extends App {
 
     val sudoku = Sudoku(sudokuSolver.returnPuzzle())
     sudoku.create()
-
   }
-  /*JFileChooser jfc = new JFileChooser(FileSystemView.getFileSystemView().getHomeDirectory());
-
-		int returnValue = jfc.showOpenDialog(null);
-		// int returnValue = jfc.showSaveDialog(null);
-
-		if (returnValue == JFileChooser.APPROVE_OPTION) {
-			File selectedFile = jfc.getSelectedFile();
-			System.out.println(selectedFile.getAbsolutePath());
-		}
-*/
-
 }
