57075ad473ed08589d2aa9f2e8a86505f2be012d
lorenz95 <lorenzMega95@outlook.it>
2020-07-31 18:24:14 +0200

    migliorato inserimento numero

--- src/main/scala/grafic/event/moduleListener/InsertNumber.scala
+++ src/main/scala/grafic/event/moduleListener/InsertNumber.scala
@@ -7,32 +7,38 @@ protected[event] object InsertNumber {
   import grafic.{tfCells, masks, cp, setPressed, getPuzzleResolt, utentSolved}
   import grafic.util.{CLOSED_CELL_BGCOLOR, CLOSED_CELL_TEXT}
 
+  private def operationOnGUI(row: Int, col: Int, t: JTextField): Unit = {
+    t.setForeground(Color.green)
+    JOptionPane.showMessageDialog(cp, "Good! The number is in Puzzle", "Message", JOptionPane.DEFAULT_OPTION)
+
+    tfCells(row)(col).setEditable(false)
+    tfCells(row)(col).setBackground(CLOSED_CELL_BGCOLOR)
+    tfCells(row)(col).setForeground(CLOSED_CELL_TEXT)
+
+    masks(row)(col) = true
+  }
+
+  private def actionUtent(): Unit = {
+    if (utentSolved()) {
+      JOptionPane.showMessageDialog(cp, "Game end, Puzzle solved", "Message", JOptionPane.DEFAULT_OPTION)
+      val option = JOptionPane.showConfirmDialog(null, "New Game?", "Message", JOptionPane.YES_NO_CANCEL_OPTION)
+      option match {
+        case 0 =>
+          cp.setVisible(false)
+          initAndUpload()
+        case 1 => System.exit(0)
+        case _ => System.out.println("cancel")
+      }
+    }
+  }
+
   def writeNumber(row: Int, col: Int, number: Int, t: JTextField): Unit = {
     getPuzzleResolt(row)(col) match {
       case `number` =>
-        t.setForeground(Color.green)
-        JOptionPane.showMessageDialog(cp, "Good! The number is in Puzzle", "Message", JOptionPane.DEFAULT_OPTION)
-
-        tfCells(row)(col).setEditable(false)
-        tfCells(row)(col).setBackground(CLOSED_CELL_BGCOLOR)
-        tfCells(row)(col).setForeground(CLOSED_CELL_TEXT)
-
-        masks(row)(col) = true
-
+        operationOnGUI(row, col, t)
         //messa a comodo
         setPressed(row, col)
-
-        if (utentSolved()) {
-          JOptionPane.showMessageDialog(cp, "Game end, Puzzle solved", "Message", JOptionPane.DEFAULT_OPTION)
-          val option = JOptionPane.showConfirmDialog(null, "New Game?", "Message", JOptionPane.YES_NO_CANCEL_OPTION)
-          option match {
-            case 0 =>
-              cp.setVisible(false)
-              initAndUpload()
-            case 1 => System.exit(0)
-            case _ => System.out.println("cancel")
-          }
-        }
+        actionUtent()
       case _ =>
         t.setForeground(Color.red)
         setPressed(-1, -1)
