5dc0d4fff0600af5dd86ad14e8f543f2a0084a39
Silvia <silvia.zandoli2@studio.unibo.it>
2020-08-02 10:05:25 +0200

    refactor

--- src/main/scala/grafic/MainGraphic.scala
+++ src/main/scala/grafic/MainGraphic.scala
@@ -1,4 +1,5 @@
 package grafic
+
 import grafic.Sudoku.Sudoku
 import resolutionAlgorithm.FullExploration
 import scalafx.Includes._
@@ -19,34 +20,32 @@ import scala.io.Source
 //interface of uploading sudokus by Zandoli
 object MainGraphic extends JFXApp {
 
-  stage=new application.JFXApp.PrimaryStage {
-    title="Menu"
-    scene=new Scene(600,300) {
+  stage = new application.JFXApp.PrimaryStage {
+    title = "Menu"
+    scene = new Scene(600, 300) {
       val menuBar = new MenuBar
       val fileMenu = new Menu("Open Sudoku")
 
-      val startItem=new MenuItem("Start Game")
-      startItem.accelerator=new KeyCodeCombination(KeyCode.Y,KeyCombination.ControlDown)
+      val startItem = new MenuItem("Start Game")
+      startItem.accelerator = new KeyCodeCombination(KeyCode.Y, KeyCombination.ControlDown)
 
       val openItem = new MenuItem("Open File")
       openItem.accelerator = new KeyCodeCombination(KeyCode.Digit0, KeyCombination.ControlDown)
 
-      val saveItem = new MenuItem("Rename File")
+      val saveItem = new MenuItem("Save File")
       saveItem.accelerator = new KeyCodeCombination(KeyCode.Digit5, KeyCombination.ControlDown)
-
 
 
       val exitItem = new MenuItem("Exit")
       exitItem.accelerator = new KeyCodeCombination(KeyCode.X, KeyCombination.ControlDown)
 
 
-      fileMenu.items = List( startItem, openItem, saveItem, new SeparatorMenuItem, exitItem)
-      val initGame=new Button("Init Game:")
+      fileMenu.items = List(startItem, openItem, saveItem, new SeparatorMenuItem, exitItem)
+      val initGame = new Button("Init Game:")
 
 
-      menuBar.menus  = List(fileMenu)
+      menuBar.menus = List(fileMenu)
       menuBar.prefWidth = 800
-
 
 
       val label = new Label("File Opened:")
@@ -54,32 +53,32 @@ object MainGraphic extends JFXApp {
       label.layoutY = 150
 
 
-      content=List(menuBar,label)
+      content = List(menuBar, label)
 
       //defining handles
-      exitItem.onAction=(event:ActionEvent) => sys.exit(0)
+      exitItem.onAction = (event: ActionEvent) => sys.exit(0)
 
       //apre il file txt e lo carica nella matrice del sudoku, inizia il gioco
-      startItem.onAction= (event:ActionEvent)=>{
+      startItem.onAction = (event: ActionEvent) => {
 
-        val fileChooser=new FileChooser{
+        val fileChooser = new FileChooser {
           title = "Open Resource File"
 
-          //you can open only file txt
+          //you can open only file .txt
           extensionFilters ++= Seq(
             new ExtensionFilter("Text Files", "*.txt"),
 
           )
 
         }
-        //you can open multiple sudoku
-        val selectedFile= fileChooser.showOpenMultipleDialog(stage)
+        //you can open multiple sudoku (you can select more than one file)
+        val selectedFile = fileChooser.showOpenMultipleDialog(stage)
 
 
-        selectedFile.foreach(i=>{
+        selectedFile.foreach(i => {
           //i.toString
 
-          label.text="Open "+ selectedFile
+          label.text = "Open " + selectedFile
           loadPuzzle(i.toString)
           initList()
           val sudokuSolver = FullExploration(getPuzzle)
@@ -93,13 +92,12 @@ object MainGraphic extends JFXApp {
         // loadPuzzle(selectedFile.toString)
 
 
-
         //areaText.
       }
 
       //apre il file txt e lo stampa su label
-      openItem.onAction= (event:ActionEvent)=>{
-        val fileChooser=new FileChooser{
+      openItem.onAction = (event: ActionEvent) => {
+        val fileChooser = new FileChooser {
           title = "Open Resource File"
 
           //you can open only file txt
@@ -110,41 +108,26 @@ object MainGraphic extends JFXApp {
 
         }
         //you can open multiple sudoku
-        val selectedFile= fileChooser.showOpenDialog(stage)
+        val selectedFile = fileChooser.showOpenDialog(stage)
         // val lines = Source.fromFile("selectedFile").getLines.toList
         /* for (line <- Source.fromFile(selectedFile).getLines) {
            println(line)
 
          }*/
         val fileContents = Source.fromFile(selectedFile).getLines.mkString
-        label.text= fileContents
+        label.text = fileContents
       }
 
-      saveItem.onAction = (event:ActionEvent) => {
-        val fileChooser=new FileChooser
-        val selectedFile=fileChooser.showSaveDialog(stage)
-        label.text="Save "+selectedFile
+      saveItem.onAction = (event: ActionEvent) => {
+        val fileChooser = new FileChooser
+        val selectedFile = fileChooser.showSaveDialog(stage)
+        label.text = "Save " + selectedFile
 
       }
     }
 
 
-
-
-
-
-
-
-
-
-
   }
-
-
-
-
-
-
 
 
 }
