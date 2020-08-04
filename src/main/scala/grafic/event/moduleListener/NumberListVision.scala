f546874cac732c688ce774c67d79c2a509410042
lorenz95 <lorenzMega95@outlook.it>
2020-08-04 13:08:53 +0200

    da fare number list vision

--- src/main/scala/grafic/event/moduleListener/NumberListVision.scala
+++ src/main/scala/grafic/event/moduleListener/NumberListVision.scala
@@ -1,9 +1,14 @@
 package grafic.event.moduleListener
 
+import javax.swing.JTextField
+
 /**
 * TODO: fare modulo che ha una funzione per aiutare
 *  l'utente a scrivere i numeri ==> @Pacini
 */
 protected[event] object NumberListVision {
 
+  def writePossibileElements(possibleValues: Set[Int], number: Int, t: JTextField): Unit = {
+
+  }
 }
