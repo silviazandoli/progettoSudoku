c1e18686947856642dccd7322a3f9cee885be8ed
Silvia <silvia.zandoli2@studio.unibo.it>
2020-09-01 01:01:34 +0200

    altre modifiche

--- src/main/scala/grafic/panels/funAux/SaveLoad.scala
+++ src/main/scala/grafic/panels/funAux/SaveLoad.scala
@@ -20,7 +20,7 @@ object SaveLoad {
   def save(): Unit = {
     FileWork.deleteFile()
     FileWork.createFile()
-    val bw = new BufferedWriter(new FileWriter(new File("input/loadOld/tmp.txt")))
+    val bw = new BufferedWriter(new FileWriter(new File("input/loadold/tmp.txt")))
 
     //you save the file
     for (i <- 0 until dimSudoku; j <- 0 until dimSudoku) {
