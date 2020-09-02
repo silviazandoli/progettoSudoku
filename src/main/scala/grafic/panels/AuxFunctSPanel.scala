abd8eb6bb229a0d45068769c61bcca4001062347
lorenz95 <lorenzMega95@outlook.it>
2020-09-02 10:18:48 +0200

    eliminato file

--- src/main/scala/grafic/panels/AuxFunctSPanel.scala
+++ src/main/scala/grafic/panels/AuxFunctSPanel.scala
@@ -1,50 +1,15 @@
-
 package grafic.panels
-
-import grafic.panels.funAux.ThreadTime
 
 object AuxFunctSPanel {
 
   import java.awt.Color
-
-  import grafic.textTime
-  import grafic.util.{AssociateListener, factSecond, score}
+  import grafic.util.{AssociateListener, factSecond}
+  import grafic.panels.funAux.ThreadTime
 
   var timeInit: Long = 0
   var stopVar = false
   var firstTime = true
 
-  //if you load a game that you stopped you have to set the old time and score too
-  /* if(load) {
-     SaveLoad.read()
-   }*/
-
-  /*
-  var thread = new Thread {
-    override def run() {
-      while (true) {
-        if (!stopVar) {
-          textTime.setText("Your Time: " + ((System.currentTimeMillis() / factSecond) - timeInit))
-        } else {
-          val text = textTime.getText()
-          if (!text.isEmpty) {
-            try {
-              val time = text.substring(11, text.length).toInt
-
-              textTime.setText("")
-              textTime.append("Your score = " + score)
-              textTime.append("\nYour time = " + time)
-
-              startStopButton.setBackground(Color.green)
-            } catch {
-              case _: NumberFormatException =>
-            }
-          }
-        }
-      }
-    }
-  }
-   */
   var thread = ThreadTime()
 
   def startStop(): Unit = {
