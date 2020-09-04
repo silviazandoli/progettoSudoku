d1598a7608b2f511cf40e70e36f31c8b3474956b
lorenz95 <lorenzMega95@outlook.it>
2020-09-04 12:23:48 +0200

    refactor

--- src/main/scala/grafic/panels/SPanel.scala
+++ src/main/scala/grafic/panels/SPanel.scala
@@ -5,51 +5,15 @@ package grafic.panels
  */
 object SPanel {
   import java.awt.event.ActionEvent
-  import java.awt.{BorderLayout, Color, Dimension, EventQueue}
+  import java.awt.{BorderLayout, Dimension, EventQueue}
   import javax.swing.JPanel
 
-  import grafic.panels.funAux.SaveLoad
-  import grafic.util.FONT_MATLIST
-  import grafic.{showNumberList, textTime, tfCells}
+  import grafic.tfCells
   import utility.dimSudoku
 
+  import grafic.panels.funAux.MakePanelGrafic._
+
   def apply(dimension: Dimension): SPanel = SPanel(dimension)
-
-  private def setDimension(): Unit = {
-    pb.setPreferredSize(new Dimension(ButtonsWidth, ButtonsHeight)); // dim
-    pb.setBackground(WS)
-
-    FL.setVgap(55)
-    FL.setHgap(100) //set the flow layout to give  symmetric display
-    pb.setLayout(FL)
-  }
-
-  private def showNumberListMake(): Unit = {
-    showNumberList.setForeground(Color.WHITE)
-    showNumberList.setBackground(Color.BLUE)
-    showNumberList.setFont(FONT_MATLIST)
-    showNumberList.setPreferredSize(new Dimension(ButtonsWidth, ButtonsHeight))
-
-    pb.add(showNumberList)
-  }
-
-  private def startStopButtonMake(): Unit = {
-    startStopButton.setBackground(Color.green)
-    startStopButton.addActionListener((_: ActionEvent) => AuxFunctSPanel.startStop())
-
-    pb.add(startStopButton)
-  }
-
-  private def addButtons(): Unit = {
-    pb.add(refreshList)
-    textTime.setPreferredSize(new Dimension(ButtonsWidth, ButtonsHeight*2)); // dim
-    pb.add(textTime)
-    saveButton.addActionListener((_: ActionEvent) => SaveLoad.save())
-    pb.add(saveButton)
-
-    adviceButton.addActionListener((_: ActionEvent) => Advice.suggerisci())
-    pb.add(adviceButton)
-  }
 
   //construct the sudoku display panel
   trait SPanelTrait extends JPanel {
---
+++ src/main/scala/grafic/panels/funAux/MakePanelGrafic.scala
@@ -0,0 +1,46 @@
+package grafic.panels.funAux
+
+object MakePanelGrafic {
+  import java.awt.event.ActionEvent
+  import java.awt.{Color, Dimension}
+
+  import grafic.util.FONT_MATLIST
+  import grafic.{showNumberList, textTime}
+  import grafic.panels._
+
+  def setDimension(): Unit = {
+    pb.setPreferredSize(new Dimension(ButtonsWidth, ButtonsHeight)); // dim
+    pb.setBackground(WS)
+
+    FL.setVgap(55)
+    FL.setHgap(100) //set the flow layout to give  symmetric display
+    pb.setLayout(FL)
+  }
+
+  def showNumberListMake(): Unit = {
+    showNumberList.setForeground(Color.WHITE)
+    showNumberList.setBackground(Color.BLUE)
+    showNumberList.setFont(FONT_MATLIST)
+    showNumberList.setPreferredSize(new Dimension(ButtonsWidth, ButtonsHeight))
+
+    pb.add(showNumberList)
+  }
+
+  def startStopButtonMake(): Unit = {
+    startStopButton.setBackground(Color.green)
+    startStopButton.addActionListener((_: ActionEvent) => AuxFunctSPanel.startStop())
+
+    pb.add(startStopButton)
+  }
+
+  def addButtons(): Unit = {
+    pb.add(refreshList)
+    textTime.setPreferredSize(new Dimension(ButtonsWidth, ButtonsHeight*2)); // dim
+    pb.add(textTime)
+    saveButton.addActionListener((_: ActionEvent) => SaveLoad.save())
+    pb.add(saveButton)
+
+    adviceButton.addActionListener((_: ActionEvent) => Advice.suggerisci())
+    pb.add(adviceButton)
+  }
+}
