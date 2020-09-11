c7c1f36ff34cac13f81caacddf3d407b8c4d9ebd
lorenz95 <lorenzMega95@outlook.it>
2020-09-11 18:42:41 +0200

    aggiunti test sul prolog

---
+++ src/test/scala/testProlog/sudokuPrologTest1.scala
@@ -0,0 +1,61 @@
+package testProlog
+
+import java.io.FileInputStream
+
+import alice.tuprolog.{Prolog, SolveInfo, Theory}
+import org.scalatest.{BeforeAndAfter, FunSuite}
+
+class sudokuPrologTest1 extends FunSuite {
+  val engine = new Prolog
+  val fileSudoku = "prolog/NuovoSudoku.pl"
+
+  engine.addTheory(new Theory(new FileInputStream(fileSudoku)))
+
+  test("TestSudoku00") {
+    val info = engine.solve("sudoku(1, 9, 6, 4, 7, 8, 3, 2, 5," +
+      "\n        3, 8, 5, 2, 6, 1, 7, 4, 9," +
+      "\n        4, 2, 7, 5, 3, _, 8, 1, 6," +
+      "\n        8, 3, 2, 6, 5, 4, 1, 9, 7," +
+      "\n        6, _, 9, _, 2, 7, 5, 3, 4," +
+      "\n        7, 5, 4, 9, 1, 3, 6, 8, 2," +
+      "\n        2, 7, 8, 1, 4, 6, 9, 5, 3," +
+      "\n        9, _, 3, 7, 8, 5, 2, 6, 1," +
+      "\n        5, 6, 1, 3, 9, 2, 4, 7, _).")
+
+    val solution = "" + info.getSolution
+
+    assert(solution == "sudoku(1,9,6,4,7,8,3,2,5,"+
+      "3,8,5,2,6,1,7,4,9," +
+      "4,2,7,5,3,9,8,1,6," +
+      "8,3,2,6,5,4,1,9,7," +
+      "6,1,9,8,2,7,5,3,4," +
+      "7,5,4,9,1,3,6,8,2," +
+      "2,7,8,1,4,6,9,5,3," +
+      "9,4,3,7,8,5,2,6,1," +
+      "5,6,1,3,9,2,4,7,8)")
+  }
+
+  test("TestSudoku01") {
+    val info = engine.solve("sudoku(1, 9, 6, 4, 7, 8, 3, 2, 5," +
+      "\n        3, 8, 5, 2, 6, 1, 7, 4, 9," +
+      "\n        4, 2, 7, 5, 3, 9, 8, 1, 6," +
+      "\n        8, 3, 2, 6, 5, 4, 1, 9, 7," +
+      "\n        6, 1, 9, _, 2, 7, 5, 3, 4," +
+      "\n        7, 5, 4, 9, 1, 3, 6, 8, 2," +
+      "\n        2, 7, 8, 1, 4, 6, 9, 5, 3," +
+      "\n        9, 4, 3, 7, 8, 5, 2, 6, 1," +
+      "\n        5, 6, 1, 3, 9, 2, 4, 7, 8).")
+
+    val solution = "" + info.getSolution
+
+    assert(solution == "sudoku(1,9,6,4,7,8,3,2,5,"+
+      "3,8,5,2,6,1,7,4,9," +
+      "4,2,7,5,3,9,8,1,6," +
+      "8,3,2,6,5,4,1,9,7," +
+      "6,1,9,8,2,7,5,3,4," +
+      "7,5,4,9,1,3,6,8,2," +
+      "2,7,8,1,4,6,9,5,3," +
+      "9,4,3,7,8,5,2,6,1," +
+      "5,6,1,3,9,2,4,7,8)")
+  }
+}
