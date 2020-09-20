62196a2e1e6f4b8fc95c974fa2e5613f3ef559f1
Silvia <silvia.zandoli2@studio.unibo.it>
2020-09-20 17:24:10 +0200

    aggiunta codice, da finire

--- src/main/scala/grafic/event/moduleListener/ControlAndFinish.scala
+++ src/main/scala/grafic/event/moduleListener/ControlAndFinish.scala
@@ -2,16 +2,21 @@ package grafic.event.moduleListener
 
 import grafic.panels.TextOpNumber.TextOpNumber
 
+/*made by Zandoli*/
+
 protected[event] object ControlAndFinish {
 
   import java.awt.Color
-
-  /*made by Zandoli*/
-
   import grafic.{cp, graficSet, utentSolved}
   import javax.swing.JOptionPane
 
-//control on number lists
+  /**
+   * Controls if a number is contained in the list of numbers of a cell and shows a messaage dialog
+   *
+   * @param possibleValues the list of numbers
+   * @param number         the number to control
+   * @param t              the cell
+   */
   def seeVisionErrors(possibleValues: Set[Int], number: Int, t: TextOpNumber): Unit = {
     if (possibleValues.contains(number)) {
       t.setForeground(Color.green)
@@ -28,7 +33,9 @@ protected[event] object ControlAndFinish {
     }
   }
 
-  //finish of the game
+  /**
+   * action when a sudoku is resolved
+   */
   def actionUtent(): Unit = {
     if (utentSolved()) {
       JOptionPane.showMessageDialog(cp, "Game end, Puzzle solved", "Message", JOptionPane.DEFAULT_OPTION)
@@ -36,15 +43,15 @@ protected[event] object ControlAndFinish {
       option match {
         case 0 =>
 
-        // reupload of the sudoku
+        // reupload of a sudoku
         //the interface of Sudoku remains open, so you don't have to write anything here
-        //when you finish the game click the exit button in the sudoku matrix and open a new file from the interface
+        //when you finish the game, and want to start another, click the exit button of the sudoku resolved
+        // and open a new file from the interface
 
         case 1 => System.exit(0)
         case _ => System.out.println("cancel")
       }
     }
   }
-
 }
 
